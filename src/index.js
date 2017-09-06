c659ff71d869cc9418faf5356a8eac598318adc5
Alexey Burtsev <alexburcev@bk.ru>
2017-09-07 02:19:45 +1000

    Подготовительные настройки
    Изменен стартовый скрипт. Убрано лишнее из компонента app. Создан файл
    редусеров

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import reducers from './reducers'
+
+import App from './App';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
+const store = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root'));
